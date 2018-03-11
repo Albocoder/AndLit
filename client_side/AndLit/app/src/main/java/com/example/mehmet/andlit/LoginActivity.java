d5e495695224d4c1147b300091a0e6e2affcb819
Albocoder <albocoder@gmail.com>
2018-03-11 21:33:05 +0300

    adding login

--- client_side/AndLit/app/src/main/java/com/example/mehmet/andlit/LoginActivity.java
+++ client_side/AndLit/app/src/main/java/com/example/mehmet/andlit/LoginActivity.java
@@ -3,7 +3,9 @@ package com.example.mehmet.andlit;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -14,13 +16,21 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.mehmet.andlit.CloudInterface.Authenticator;
+import com.example.mehmet.andlit.database.entities.UserLogin;
+
+import java.io.IOException;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+    // constants
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_SIGNUP = 0;
 
+    // fields
+    private Authenticator a;
     @BindView(R.id.input_email)
     EditText _emailText;
     @BindView(R.id.input_password)
@@ -30,19 +40,27 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.link_signup)
     TextView _signupLink;
 
-    AnimationDrawable animationDrawable;
-    ScrollView sv;
+    // view related
+    private AnimationDrawable animationDrawable;
+    private ScrollView sv;
 
+
+    // ******************************** VIEW RELATED FUNCTIONS ******************************** //
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        a = new Authenticator(this);
+
+        if(a.isLoggedIn())
+            goToHomePage();
 
         sv = (ScrollView)findViewById(R.id.loginLayout);
         animationDrawable = (AnimationDrawable) sv.getBackground();
         animationDrawable.setEnterFadeDuration(5000);
         animationDrawable.setExitFadeDuration(2000);
+
 
 
         _loginButton.setOnClickListener(new View.OnClickListener() {
@@ -65,6 +83,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -79,7 +98,24 @@ public class LoginActivity extends AppCompatActivity {
             animationDrawable.stop();
     }
 
+    // if signup is successful we go to home page and finish this
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_SIGNUP) {
+            if (resultCode == RESULT_OK) {
+                goToHomePage();
+                this.finish();
+            }
+        }
+    }
 
+    @Override
+    public void onBackPressed() {
+        // Disable going back to the MainActivity
+        moveTaskToBack(true);
+    }
+
+    // ****************************** OPERATIONS RELATED FUNCTIONS ****************************** //
     public void login() {
         Log.d(TAG, "Login");
 
@@ -96,49 +132,25 @@ public class LoginActivity extends AppCompatActivity {
         progressDialog.setMessage("Authenticating...");
         progressDialog.show();
 
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
+        final String username = _emailText.getText().toString();
+        final String password = _passwordText.getText().toString();
 
-        // TODO: Implement your own authentication logic here.
-
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onLoginSuccess or onLoginFailed
-                        onLoginSuccess();
-                        // onLoginFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
+        new LoginTask().execute(username,password);
     }
 
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_SIGNUP) {
-            if (resultCode == RESULT_OK) {
-
-                // TODO: Implement successful signup logic here
-                // By default we just finish the Activity and log them in automatically
-                this.finish();
-            }
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        // Disable going back to the MainActivity
-        moveTaskToBack(true);
+    private void goToHomePage() {
+        // todo: add logic for going to home page
+        finish();
     }
 
     public void onLoginSuccess() {
         _loginButton.setEnabled(true);
+        goToHomePage();
         finish();
     }
 
     public void onLoginFailed(String msg) {
         Toast.makeText(getBaseContext(), "Error: "+msg, Toast.LENGTH_LONG).show();
-
         _loginButton.setEnabled(true);
     }
 
@@ -163,5 +175,42 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         return valid;
+    }
+
+    private class LoginTask extends AsyncTask<String, Void, Void>
+    {
+
+        private ProgressDialog Dialog = new ProgressDialog(LoginActivity.this,R.style.AppTheme_Dark_Dialog);
+
+        protected void onPreExecute() {
+            // Display the loading spinner
+            Dialog.setMessage("Authenticating.. ");
+            Dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            Dialog.setInverseBackgroundForced(false);
+            Dialog.setIndeterminate(true);
+            Dialog.setCancelable(false);
+            Dialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(String... paramsObj) {
+            try {
+                UserLogin ul = a.login(paramsObj[0],paramsObj[1]);
+                if ( ul == null ) {
+                    Log.d(TAG,"Wrong credentials!");
+                    return null;
+                }
+            } catch ( IOException e ) {
+                Log.d(TAG,"No internet connection!");
+                return null;
+            }
+            // On complete call either onLoginSuccess or onLoginFailed
+            onLoginSuccess();
+            return null;
+        }
+
+        protected void onPostExecute(Void unused) {
+            Dialog.dismiss();
+        }
     }
 }
