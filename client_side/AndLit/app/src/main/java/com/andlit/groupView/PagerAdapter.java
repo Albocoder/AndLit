4b24bd3d216f4f7f30c4e6aab699db0180fb1b35
Muhammad Hamza Khan <muhammadhamzakhan008@gmail.com>
2018-04-26 18:00:02 +0300

    front-end of groups implemented

--- client_side/AndLit/app/src/main/java/com/andlit/groupView/PagerAdapter.java
+++ client_side/AndLit/app/src/main/java/com/andlit/groupView/PagerAdapter.java
@@ -20,13 +20,13 @@ public class PagerAdapter extends FragmentStatePagerAdapter
         switch( position )
         {
             case 0:
-                TabFragment1 tab1 = new TabFragment1();
+                YourGroupsFragment tab1 = new YourGroupsFragment();
                 return tab1;
             case 1:
-                TabFragment2 tab2 = new TabFragment2();
+                JoinGroupFragment tab2 = new JoinGroupFragment();
                 return tab2;
             case 2:
-                TabFragment3 tab3 = new TabFragment3();
+                CreateGroupFragment tab3 = new CreateGroupFragment();
                 return tab3;
             default:
                 return null;
