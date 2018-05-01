ba2e65367e577cdc5a529994b93b0aea8e0a786f
Muhammad Hamza Khan <muhammadhamzakhan008@gmail.com>
2018-05-01 18:50:45 +0300

    KnownPeopleView bug fixed

--- client_side/AndLit/app/src/main/java/com/andlit/ui/knownPeopleView/KnownPeopleViewRVAdapter.java
+++ client_side/AndLit/app/src/main/java/com/andlit/ui/knownPeopleView/KnownPeopleViewRVAdapter.java
@@ -1,6 +1,8 @@
 package com.andlit.ui.knownPeopleView;
 
+import android.content.Context;
 import android.view.View;
+import com.andlit.database.AppDatabase;
 import com.andlit.database.entities.KnownPPL;
 import com.andlit.ui.trainingView.TrainingViewRVAdapter;
 import java.util.List;
@@ -20,6 +22,8 @@ public class KnownPeopleViewRVAdapter extends TrainingViewRVAdapter
     @Override
     public void onBindViewHolder(PersonViewHolder personViewHolder, final int position)
     {
+        final Context context = personViewHolder.itemView.getContext();
+
         KnownPPL person = allKnownPpl.get(position);
         personViewHolder.personName.setText(person.name + " " + person.sname);
         personViewHolder.personAge.setText(person.age + " years old");
@@ -31,26 +35,13 @@ public class KnownPeopleViewRVAdapter extends TrainingViewRVAdapter
             public void onClick(View view)
             {
                 // Remove from database as well
-
+                AppDatabase db = AppDatabase.getDatabase(context);
+                db.knownPplDao().deletePerson(allKnownPpl.get(position));
 
                 // Remove the item on remove button click
-                persons.remove(position); // TODO: fix this bug here when you click delete
-                /*
-        java.lang.NullPointerException: Attempt to invoke interface method 'java.lang.Object java.util.List.remove(int)' on a null object reference
-        at com.andlit.ui.knownPeopleView.KnownPeopleViewRVAdapter$1.onClick(KnownPeopleViewRVAdapter.java:37)
-        at android.view.View.performClick(View.java:5637)
-        at android.view.View$PerformClick.run(View.java:22433)
-        at android.os.Handler.handleCallback(Handler.java:751)
-        at android.os.Handler.dispatchMessage(Handler.java:95)
-        at android.os.Looper.loop(Looper.java:179)
-        at android.app.ActivityThread.main(ActivityThread.java:6152)
-        at java.lang.reflect.Method.invoke(Native Method)
-        at com.android.internal.os.ZygoteInit$MethodAndArgsCaller.run(ZygoteInit.java:886)
-        at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:776)
-
-                * */
+                allKnownPpl.remove(position);
                 notifyItemRemoved(position);
-                notifyItemRangeChanged(position, persons.size());
+                notifyItemRangeChanged(position, allKnownPpl.size());
             }
         });
 
@@ -61,6 +52,7 @@ public class KnownPeopleViewRVAdapter extends TrainingViewRVAdapter
             public void onClick(View view)
             {
                 // TODO: 4/29/18 pop up appearance for edit info
+                
             }
         });
     }
